d1579bf1ee47162876d3f64022e4afe71e4a78de
Everton Santos <everton-r.santos@hotmail.com>
2022-05-29 00:53:31 -0300

    Add Entidades

---
+++ src/Projeto.MVC/Projeto.MVC.Rest/Migrations/20220529025701_Initial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Projeto.MVC.Rest.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Department");
+        }
+    }
+}
---
+++ src/Projeto.MVC/Projeto.MVC.Rest/Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using Projeto.MVC.Rest.Models.Enums;
+using System;
+
+namespace Projeto.MVC.Rest.Models
+{
+    public class SalesRecord
+    {
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(Guid id, DateTime date, double amount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+
+}
