129ec009ea4d2156c2c9233094861db00ebe8a2d
Everton Santos <everton-r.santos@hotmail.com>
2022-05-30 01:36:14 -0300

    Delete Seller

--- src/Projeto.MVC/Projeto.MVC.Rest/Startup.cs
+++ src/Projeto.MVC/Projeto.MVC.Rest/Startup.cs
@@ -63,7 +63,7 @@ namespace Projeto.MVC.Rest
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Departments}/{action=Index}/{id?}");
             });
         }
     }
